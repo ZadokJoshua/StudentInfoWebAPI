28641dbb16494fceb79de56d329f965e22387283
Zadok Joshua <Ayegbazadok@outlook.com>
2022-09-22 03:29:48 +0100

    Add project files.

---
+++ StudentInfoApi/Models/CourseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentInfoAPI.Models
+{
+    public class CourseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public char Grade { get; set; }
+    }
+}
---
+++ StudentInfoApi/Models/StudentDto.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentInfoAPI.Models
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public int CoursesCount
+        {
+            get
+            {
+                return Courses.Count;
+            }
+        }
+
+        public ICollection<CourseDto> Courses = new List<CourseDto>();
+    }
+}
